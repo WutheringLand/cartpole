df9c4955336e831015f545c26a125390ea8e0af8
WutheringLand <31542342+WutheringLand@users.noreply.github.com>
2020-03-14 09:29:36 -0400

    change

--- scores/score_logger.py
+++ scores/score_logger.py
@@ -28,14 +28,14 @@ class ScoreLogger:
 
     def add_score(self, score, run):
         self._save_csv(SCORES_CSV_PATH, score)
-        # self._save_png(input_path=SCORES_CSV_PATH,
-        #                 output_path=SCORES_PNG_PATH,
-        #                 x_label="runs",
-        #                 y_label="scores",
-        #                 average_of_n_last=CONSECUTIVE_RUNS_TO_SOLVE,
-        #                 show_goal=True,
-        #                 show_trend=True,
-        #                 show_legend=True)
+#        self._save_png(input_path=SCORES_CSV_PATH,
+#                         output_path=SCORES_PNG_PATH,
+#                         x_label="runs",
+#                         y_label="scores",
+#                         average_of_n_last=CONSECUTIVE_RUNS_TO_SOLVE,
+#                         show_goal=True,
+#                         show_trend=True,
+#                         show_legend=True)
         self.scores.append(score)
         mean_score = mean(self.scores)
         print("Scores: (min: " + str(min(self.scores)) + ", avg: " + str(mean_score) + ", max: " + str(max(self.scores)) + ")\n")
@@ -43,14 +43,14 @@ class ScoreLogger:
             solve_score = run-CONSECUTIVE_RUNS_TO_SOLVE
             print("Solved in " + str(solve_score) + " runs, " + str(run) + " total runs.")
             self._save_csv(SOLVED_CSV_PATH, solve_score)
-            self._save_png(input_path=SOLVED_CSV_PATH,
-                           output_path=SOLVED_PNG_PATH,
-                           x_label="trials",
-                           y_label="steps before solve",
-                           average_of_n_last=None,
-                           show_goal=False,
-                           show_trend=False,
-                           show_legend=False)
+#            self._save_png(input_path=SOLVED_CSV_PATH,
+#                           output_path=SOLVED_PNG_PATH,
+#                           x_label="trials",
+#                           y_label="steps before solve",
+#                           average_of_n_last=None,
+#                           show_goal=False,
+#                           show_trend=False,
+#                           show_legend=False)
             exit()
 
     def _save_png(self, input_path, output_path, x_label, y_label, average_of_n_last, show_goal, show_trend, show_legend):
@@ -59,7 +59,7 @@ class ScoreLogger:
         with open(input_path, "r") as scores:
             reader = csv.reader(scores)
             data = list(reader)
-            for i in range(0, len(data)):
+            for i in range(0, len(data)-1):
                 x.append(int(i))
                 y.append(int(data[i][0]))
 
@@ -92,7 +92,7 @@ class ScoreLogger:
         if not os.path.exists(path):
             with open(path, "w"):
                 pass
-        scores_file = open(path, "a")
-        with scores_file:
+#        scores_file = open(path, "a", newline = "")
+        with open(path, 'a') as scores_file:
             writer = csv.writer(scores_file)
             writer.writerow([score])
